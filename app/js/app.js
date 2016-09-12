ce55cefe5c85269f3c71ec0ef140fe2a7d3a8bcd
Milos Pavlicevic <milos.pavlicevic@null-object.com>
2016-09-12 18:25:03 +0200

    init

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Menu from './components/Global/Menu.jsx';
+
+var Menu = function() {
+    return (
+        <div>YO</div>
+    );
+};
+
+ReactDOM.render(
+  <Menu />,
+  document.getElementById('app')
+);
